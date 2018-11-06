0bcc98765c9f69023cc026229f329c6b0df7621c
sfpuebla <sfpuebla@gmail.com>
2018-11-06 18:45:09 +0100

    PAC 3 Ejercicio 1

---
+++ app/src/main/java/com/edu/uoc/mybooks/MyFirebaseMessagingService.java
@@ -0,0 +1,49 @@
+package com.edu.uoc.mybooks;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Método llamado cuando se recibe un mensaje remoto
+     *
+     * @param remoteMessage Mensaje recibido de Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Mostrar una notificación al recibir un mensaje de Firebase
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    /**
+     * Crea y muestra una notificación al recibir un mensaje de Firebase
+     *
+     * @param messageBody Texto a mostrar en la notificación
+     */
+    private void sendNotification(String messageBody) {
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Ejemplo Firebase")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
