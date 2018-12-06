80aad466c5d10369dc54ce398659a55add19586e
sfernandez <sfpuebla@gmail.com>
2018-12-06 20:11:33 +0100

    PAC4 Ejercicio 1

--- app/src/main/java/com/edu/uoc/mybooks/BookListActivity.java
+++ app/src/main/java/com/edu/uoc/mybooks/BookListActivity.java
@@ -1,10 +1,14 @@
 package com.edu.uoc.mybooks;
 
+import android.app.ActionBar;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -71,6 +76,10 @@ public class BookListActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private DatabaseReference mReference;
+    private DrawerLayout drawerLayout;
+    private android.support.v7.app.ActionBar actionBar;
+    private boolean mDrawerLayoutDisplayed = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +90,17 @@ public class BookListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
+
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            // setupNavigationDrawerContent(navigationView);
+        }
 
 
         if (getIntent() != null && getIntent().getAction() != null) {
@@ -148,7 +168,7 @@ public class BookListActivity extends AppCompatActivity {
         setupRecyclerView((RecyclerView) recyclerView);
 
         // Obtenemos los datos desde la base de datos Firebase
-            GetDataFromFirebase();
+        GetDataFromFirebase();
 
 
         final SwipeRefreshLayout swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
@@ -166,10 +186,78 @@ public class BookListActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Despliego el navigation drawer si se presiona sobre el botón de las tres rayas en la Toolbar
+                if (mDrawerLayoutDisplayed) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+
+                mDrawerLayoutDisplayed = !mDrawerLayoutDisplayed;
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*
+    private void setupNavigationDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        textView = (TextView) findViewById(R.id.textView);
+                        switch (menuItem.getItemId()) {
+                            case R.id.item_navigation_drawer_inbox:
+                                menuItem.setChecked(true);
+                                textView.setText(menuItem.getTitle());
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_starred:
+                                menuItem.setChecked(true);
+                                textView.setText(menuItem.getTitle());
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_sent_mail:
+                                menuItem.setChecked(true);
+                                textView.setText(menuItem.getTitle());
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_drafts:
+                                menuItem.setChecked(true);
+                                textView.setText(menuItem.getTitle());
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_settings:
+                                menuItem.setChecked(true);
+                                textView.setText(menuItem.getTitle());
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                                startActivity(intent);
+                                return true;
+                            case R.id.item_navigation_drawer_help_and_feedback:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    */
+
     protected void closeNotification() {
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.cancel(0);
     }
+
 
     protected void showDetail(String bookPosition) {
 
